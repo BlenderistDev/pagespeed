429c0c657ef4718b48b8f291299c266cf3764823
blenderist <demonx.blenderist@gmail.com>
2020-12-28 22:23:26 +0300

    Добавлены тесты для проверки пагинации замеров

--- tests/Unit/App/Http/Controllers/MeasurementsControllerTest.php
+++ tests/Unit/App/Http/Controllers/MeasurementsControllerTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit\App\Http\Controllers;
 
+use App\Models\Measurements;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
 class MeasurementsControllerTest extends TestCase
@@ -12,5 +14,26 @@ class MeasurementsControllerTest extends TestCase
     public function testIndexResponseOk()
     {
         $this->post('/api/measurements')->assertOk();
+    }
+
+    public function testPagination()
+    {
+        $onPage = 5;
+        Event::fake();
+        Measurements::factory()->count(20)->create();
+        $response = $this->post('/api/measurements', [
+            'page' => [
+                'onPage' => $onPage
+            ],
+        ]);
+        $response->assertJsonCount($onPage, 'data');
+    }
+
+    public function testAllCount()
+    {
+        $total = 20;
+        Event::fake();
+        Measurements::factory()->count($total)->create();
+        $this->post('/api/measurements')->assertJsonFragment(['total' => $total]);
     }
 }
