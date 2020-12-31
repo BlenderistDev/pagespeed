6766ff2b026ffac9cf92315c49a6fb2d2a274e58
filippov <filippov@web-canape.com>
2020-12-31 13:12:43 +0300

    Добавление тестов для регулярных аудитов

--- tests/Unit/App/Http/Controllers/RegularAuditsControllerTest.php
+++ tests/Unit/App/Http/Controllers/RegularAuditsControllerTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Unit\App\Http\Controllers;
 
+use App\Models\RegularAudits;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
+use Faker;
 
 class RegularAuditsControllerTest extends TestCase
 {
@@ -19,5 +21,22 @@ class RegularAuditsControllerTest extends TestCase
     {
         $this->getJson('/api/regular-audits')->assertUnauthorized();
         $this->putJson('/api/regular-audits')->assertUnauthorized();
+    }
+
+    public function testIndex()
+    {
+        $faker = Faker\Factory::create();
+        $count = $faker->randomDigit;
+        $this->withoutMiddleware();
+        RegularAudits::factory()->count($count)->create();
+        $this->get('/api/regular-audits')->assertJsonCount($count);
+    }
+
+    public function testStore()
+    {
+        $this->withoutMiddleware();
+        $regularAudit = RegularAudits::factory()->stars()->make();
+        $this->put('/api/regular-audits', $regularAudit->getAttributes());
+        $this->assertDatabaseHas('regular_audits', $regularAudit->getAttributes());
     }
 }
