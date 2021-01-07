805d5e19797f4882f51baf2a72e962896f46c65c
blenderist <demonx.blenderist@gmail.com>
2021-01-07 19:52:08 +0300

    В письме о регулярном аудите отображаются очки мобильного и компьютерного замеров

--- resources/views/emails/regularAuditComplete.blade.php
+++ resources/views/emails/regularAuditComplete.blade.php
@@ -1,1 +1,19 @@
-Был проведен регулярный замер для сайта {{ $measurement->domain }}
+<div>
+    Был проведен регулярный замер для сайта {{ $measurement->domain }}
+</div>
+<div>
+    На мобильном устройстве:
+    @foreach ($measurement->measure as $measure)
+        @if ($measure->audit->name === 'score')
+            {{ $measure->value * 100 }} баллов
+        @endif
+    @endforeach
+</div>
+<div>
+    На компьютере:
+    @foreach ($measurement->measureDesktop as $measure)
+        @if ($measure->audit->name === 'score')
+            {{ $measure->value * 100 }} баллов
+        @endif
+    @endforeach
+</div>
